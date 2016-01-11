39fa1453df166653ee211c7ac70e267ae60734a3
Norrathep Rattanavipanon <superoak90@gmail.com>
2016-01-10 18:54:55 -0800

    add parser with some unit tests

---
+++ src/com/uci/ccai6/exception/ParserException.java
@@ -0,0 +1,14 @@
+package com.uci.ccai6.exception;
+
+public class ParserException extends Exception {
+	
+	public ParserException() { super("invalid syntax"); }
+	public ParserException(String message) { super("invalid syntax: "+message); }
+	public ParserException(String expected, String result) {
+		super("Expect: "+expected+" but it is "+result);
+	}
+	public ParserException(String parserFunctionName, String expected, String result) {
+		super(parserFunctionName+": Expect "+expected+" but it is "+result);
+	}
+
+}
