adc39411cedde99935ef6cd7cda77869a1b5f347
Norrathep Rattanavipanon <superoak90@gmail.com>
2016-01-27 20:37:43 -0800

    assignDestination supports pointer and relOp

--- src/edu/uci/ccai6/cs241/AssignDestination.java
+++ src/edu/uci/ccai6/cs241/AssignDestination.java
@@ -3,15 +3,31 @@ package edu.uci.ccai6.cs241;
 public class AssignDestination {
 	private boolean __isArray = false;
 	private boolean __isConstant = false;
+	private boolean __isPointer = false;
 	private String __destination = "";
+	private String __relOp = null;
 	
 	public AssignDestination(String str) {
 		__destination = str;
 	}
 	
+	public AssignDestination(long pc) {
+		__destination = "("+Long.toString(pc)+")";
+		__isPointer = true;
+	}
+	
 	public void setDestination(String str) {
 		__destination = str;
 	}
+	
+	public void setRelOp(String ro) {
+		__relOp = ro;
+	}
+	
+	public String getRelOp() {
+		return __relOp;
+	}
+	
 	public String getDestination() {
 		return __destination;
 	}
@@ -26,5 +42,8 @@ public class AssignDestination {
 	}
 	public boolean isConstant() {
 		return __isConstant;
+	}
+	public boolean isPointer() {
+		return __isPointer;
 	}
 }
