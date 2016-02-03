fbdec4e5d181fcba85493bb1f2fd408ce123f771
Oak <nrattana@uci.edu>
2016-02-03 13:36:30 -0800

    Add type

--- src/edu/uci/ccai6/cs241/AssignDestination.java
+++ src/edu/uci/ccai6/cs241/AssignDestination.java
@@ -4,22 +4,31 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AssignDestination {
+  
+    public enum Type {
+      ARRAY, CONSTANT, POINTER, VARIABLE
+    }
     // TODO: phi for array dont work...
-	private boolean __isArray = false;
-	private boolean __isConstant = false;
-	private boolean __isPointer = false;
+    private Type type = null;
 	private String __destination = "";
 	private String __relOp = null;
+	
+	// this contains all used "variables", which can be combined 
+	// with other AssignDestination to grow this set
+	// We will use this to keep track on
+	// 1) all variables assigned in both "if" branches -> create phi at the end
+	// 2) all variables "used" in while relation -> create phi at the beginning
 	private Set<String> __assignedVars = new HashSet<String>();
 	
 	public AssignDestination(String str) {
 		__destination = str;
 		__assignedVars.add(str);
+		type = Type.VARIABLE;
 	}
 	
 	public AssignDestination(long pc) {
 		__destination = "("+Long.toString(pc)+")";
-		__isPointer = true;
+		type = Type.POINTER;
 	}
 	
 	public void setDestination(String str) {
@@ -38,30 +47,33 @@ public class AssignDestination {
 		return __destination;
 	}
 	public void setIsArray(boolean isTure) {
-		__isArray = isTure;
+		type = Type.ARRAY;
 		__assignedVars = new HashSet<String>();
 	}
 	public boolean isArray() {
-		return __isArray;
+		return type == Type.ARRAY;
 	}
 	public void setIsConstant(boolean isTrue) {
-		__isConstant = isTrue;
+		type = Type.CONSTANT;
 	}
 	public boolean isConstant() {
-		return __isConstant;
+		return type == Type.CONSTANT;
 	}
 	public boolean isPointer() {
-		return __isPointer;
+		return type == Type.POINTER;
 	}
 	public Set<String> getAssignedVars() {
 	  return __assignedVars;
 	}
 
+	// join Vars
     public AssignDestination join(AssignDestination two) {
       if(two == null && this != null) return this;
       __assignedVars.addAll(two.getAssignedVars());
       return this;
     }
+    
+    // intersect Vars
     public AssignDestination intersectVars(AssignDestination two) {
       if((this == null && two != null) || (two == null && this != null)) {
         this.__assignedVars = new HashSet<>();
