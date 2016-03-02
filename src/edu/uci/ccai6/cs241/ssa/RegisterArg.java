499062e612d944a5a935b2db6f965d3e5f682134
Norrathep Rattanavipanon <superoak90@gmail.com>
2016-03-02 10:56:39 -0800

    RegisterArg

---
+++ src/edu/uci/ccai6/cs241/ssa/RegisterArg.java
@@ -0,0 +1,33 @@
+package edu.uci.ccai6.cs241.ssa;
+
+public class RegisterArg extends Arg {
+
+	int num;
+	  
+  public RegisterArg(int n) {
+    num = n;
+  }
+  
+  public RegisterArg clone() {
+    return new RegisterArg(num);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+      if(o == null || !(o instanceof RegisterArg)) return false;
+      RegisterArg two = (RegisterArg) o;
+      return num == two.num;
+  }
+  
+  public String toString() {
+    return num+"";
+  }
+
+  @Override
+  public int hashCode() {
+      int hash = 2;
+      hash = hash*5+0;
+      hash = hash*17+num;
+      return hash;
+  }
+}
